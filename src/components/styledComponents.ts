7be26b8dd57b50a51b163b83c72ce95041bec15b
exigoll <exigoll@bk.ru>
2023-02-22 03:45:25 +0500

    fix: bugs in Registration page

--- src/components/styledComponents.ts
+++ src/components/styledComponents.ts
@@ -1,6 +1,8 @@
+import { MenuOpen } from "@mui/icons-material";
 import {
   Button,
   Dialog,
+  Drawer,
   FormControlLabel,
   Select,
   TextField,
@@ -111,7 +113,73 @@ export const StyledCheckbox = styled(FormControlLabel)({
     top: "-7px",
   },
 
+  "& .MuiButtonBase-root": {
+    //padding: "0",
+  },
+
   "& .MuiFormControlLabel-label": {
     fontSize: "14px",
+    lineHeight: "14px",
+
+    /* "@media (max-width: 768px)": {
+      fontSize: "12px",
+      lineHeight: "12px",
+    }, */
+  },
+
+  "& .MuiSvgIcon-root": {
+    width: "18px",
+    height: "18px",
+  },
+});
+
+export const StylesDrawer = styled(Drawer)({
+  top: "70px",
+
+  "@media (max-width: 768px)": {
+    top: "112px",
+  },
+
+  "& .MuiDrawer-paper": {
+    top: "70px",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    rowGap: "28px",
+    width: "340px",
+    height: "100%",
+    backgroundColor: "#1b2f52",
+    borderRadius: "0",
+    boxShadow: "none",
+
+    "@media (max-width: 768px)": {
+      top: "112px",
+    },
+  },
+  "& .MuiModal-backdrop": {
+    top: "70px",
+
+    "@media (max-width: 768px)": {
+      top: "112px",
+    },
+  },
+});
+
+export const StyledMenuOpen = styled(MenuOpen)({
+  width: "40px",
+  height: "40px",
+  fill: "white",
+  cursor: "pointer",
+  transition: "all 0.3s ease-in-out",
+
+  "@media (min-width: 769px)": {
+    "&:hover": {
+      fill: "rgb(231 124 88)",
+    },
+  },
+
+  "@media (max-width: 490px)": {
+    width: "30px",
+    height: "30px",
   },
 });
--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -1,65 +1,13 @@
-import { MenuOpen } from "@mui/icons-material";
-import { Drawer, styled } from "@mui/material";
 import { FC, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 import { IconLogo } from "@/components/icons";
+import { StyledMenuOpen, StylesDrawer } from "@/components/styledComponents";
 import { Contacts } from "@/components/ui/Contacts";
 
 import { links } from "@/store/navigationItems";
 
 import styles from "./Navbar.module.scss";
-
-const StylesDrawer = styled(Drawer)({
-  top: "70px",
-
-  "@media (max-width: 768px)": {
-    top: "112px",
-  },
-
-  "& .MuiDrawer-paper": {
-    top: "70px",
-    display: "flex",
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-    rowGap: "28px",
-    width: "340px",
-    height: "100%",
-    backgroundColor: "#1b2f52",
-    borderRadius: "0",
-    boxShadow: "none",
-
-    "@media (max-width: 768px)": {
-      top: "112px",
-    },
-  },
-  "& .MuiModal-backdrop": {
-    top: "70px",
-
-    "@media (max-width: 768px)": {
-      top: "112px",
-    },
-  },
-});
-
-const StyledMenuOpen = styled(MenuOpen)({
-  width: "40px",
-  height: "40px",
-  fill: "white",
-  cursor: "pointer",
-  transition: "all 0.3s ease-in-out",
-
-  "@media (min-width: 769px)": {
-    "&:hover": {
-      fill: "rgb(231 124 88)",
-    },
-  },
-
-  "@media (max-width: 490px)": {
-    width: "30px",
-    height: "30px",
-  },
-});
 
 export const Navbar: FC = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
