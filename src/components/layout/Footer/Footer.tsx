56e3312e3610b790fc1f974b75e1009a81ffbb81
exigoll <exigoll@bk.ru>
2023-02-23 07:24:50 +0500

    test: NavBar

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -5,7 +5,11 @@ import styles from "./Footer.module.scss";
 export const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <p>© 2023 ROTAZAP.RU - Интернет-магазин запчастей для иномарок</p>
+      <div className="container">
+        <p className={styles.text}>
+          © 2023 ROTAZAP.RU - Интернет-магазин запчастей для иномарок
+        </p>
+      </div>
     </footer>
   );
 };
