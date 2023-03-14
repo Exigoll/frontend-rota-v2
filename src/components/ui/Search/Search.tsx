5387b70a2198fe286b3fc436f7df166c3590b127
exigoll <exigoll@bk.ru>
2023-03-14 22:00:39 +0500

    fix

--- src/components/ui/Search/Search.tsx
+++ src/components/ui/Search/Search.tsx
@@ -5,7 +5,7 @@ import styles from "./Search.module.scss";
 export const Search = () => {
   return (
     <div className={styles.search}>
-      <input type="text" placeholder="Введите VIN номер детали" />
+      <input type="text" placeholder="Введите артикул детали" />
       <button type="submit">
         <SearchIcon />
       </button>
