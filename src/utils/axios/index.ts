30fe2b94c96e7274514434310261301eb9fc98bb
exigoll <exigoll@bk.ru>
2023-04-13 06:49:18 +0500

    fix

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:3002",
+  baseURL: "http://localhost:3000", // заменить на 3002 для разработки
   timeout: 2000,
   headers: { "X-Custom-Header": "foobar" },
 });
